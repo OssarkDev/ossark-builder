70c38687b6a1147608079ba07ee3e358ca47bf8b
ossianmoore <ossianmoore@gmail.com>
2023-07-15 12:59:26 +0200

    Initial setup

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import 'slick-carousel/slick/slick.scss';
+// import 'slick-carousel';
+// import 'vanilla-lazyload';
+
+import "./scss/index.scss"; // custom styles
+
+import { runAfterDomLoad } from './js';
+
+document.addEventListener('DOMContentLoaded', runAfterDomLoad);
