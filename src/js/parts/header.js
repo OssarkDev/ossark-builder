dd11f3a42915527050c5bd46dfe08e7eff2c53ee
ossianmoore <ossianmoore@gmail.com>
2023-07-29 14:34:19 +0200

    Added header animation

--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -1,9 +1,9 @@
 // adds class to header when scrolled
 
 export function headerAnimation() {
-    var prevScrollpos = window.pageYOffset;
+    var prevScrollpos = window.scrollY;
     window.onscroll = function () {
-        var currentScrollPos = window.pageYOffset;
+        var currentScrollPos = window.scrollY;
         var header = document.querySelector("header");
         if (prevScrollpos > currentScrollPos) {
             header.classList.remove("hide-header");
