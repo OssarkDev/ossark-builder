663e50f61b33652dbb085d3253195addfc64e103
Ossian Moore <43648387+ossianmoore@users.noreply.github.com>
2024-07-26 16:52:51 +0100

    Working AJAX Example

--- include/setup_theme.php
+++ include/setup_theme.php
@@ -213,18 +213,13 @@ add_filter('wpcf7_autop_or_not', '__return_false');
 //   add_action( 'after_setup_theme', 'my_enable_debug_mode' );
 
 //   function my_enable_debug_mode() {
-
 //     // Turn on error reporting.
 //     error_reporting( E_ALL );
-
 //     // Sets to display errors on screen. Use 0 to turn off.
 //     ini_set( 'display_errors', 1 );
-
 //     // Sets to log errors. Use 0 (or omit) to not log errors.
 //     ini_set( 'log_errors', 1 );
-
 //     // Sets a log file path you can access in the theme editor.
 //     $log_path = get_template_directory() . '/debug.txt';
 //     ini_set( 'error_log', $log_path );
-
-//   }
+// }
--- src/js/components/parts/testAjax.js
+++ src/js/components/parts/testAjax.js
@@ -1,27 +1,42 @@
 import $ from 'jquery';
 
 export function testAjax() {
-    // var block = $(this);
-    // var container = block.find('.ajax-container');
-    // let option = block.find('.ajax-select');
-    // let id = option.val(); // Get the selected value
+    var block = $('.ajax-example');
+    var container = block.find('.ajax-container');
+    var button = block.find('.ajax-button');
+    var id = 0;
+    var data = {};
 
-    var data = {
-        action: 'test_ajax',
-        id: '1',
-    };
+    function getAjaxData() {
+        data.action = 'test_ajax';
+        data.id = id;
 
-    $.ajax({
-        url: customjs_ajax_object.ajax_url,
-        type: 'POST',
-        data: data,
-        success: function(response) {
-            if (response) {
-                console.log(response);
+        container.addClass('loading');
+
+        $.ajax({
+            url: customjs_ajax_object.ajax_url,
+            type: 'POST',
+            data: data,
+            // on success, replace the content of the container with the response
+            success: function(response) {
+                if (response) {
+                    container.html('');
+                    container.html(response);
+                    setTimeout(function() {
+                        container.removeClass('loading');
+                    }, 500);
+                }
+            },
+            error: function(error) {
+                console.error(error);
             }
-        },
-        error: function(error) {
-            console.error(error);
-        }
+        });
+    }
+
+    // When the button is clicked, get the data-id attribute and assign it to the 'id' variable and call the 'getAjaxData' function
+    button.on('click', function() {
+        var buttonId = $(this).data('id');
+        id = buttonId;
+        getAjaxData();
     });
 }
